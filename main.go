310cea970cf84d85efbb8772a660263c97ef96e0
Derek Pedersen <derekpedersen.com@gmail.com>
2022-03-03 15:13:44 -0800

    working on options now

--- main.go
+++ main.go
@@ -10,25 +10,5 @@ import (
 func main() {
 	flag.Parse()
 	log.SetLevel(log.DebugLevel)
-	auth := twitter.NewTwitterAuth()
-	user, err := twitter.NewTwitterUser(auth)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.WithFields(log.Fields{
-		"twitter user": user,
-	}).Debug()
-
-	newTweets, err := twitter.NewTweets(auth, user, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	_ = newTweets.UnRetweet()
-	for len(newTweets.Meta.NextToken) > 0 {
-		newTweets, err = twitter.NewTweets(auth, user, &newTweets.Meta.NextToken)
-		if err != nil {
-			log.Fatal(err)
-		}
-		_ = newTweets.UnRetweet()
-	}
+	twitter.Twitter()
 }
--- twitter/main.go
+++ twitter/main.go
@@ -4,7 +4,27 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func main() {
-	log.SetLevel(log.DebugLevel) // TODO: make this a flag
-	log.Infof("log level: %v", log.GetLevel())
+func Twitter() {
+	log.Info("Twitter")
+	auth := NewTwitterAuth()
+	user, err := NewTwitterUser(auth)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.WithFields(log.Fields{
+		"twitter user": user,
+	}).Debug()
+
+	newTweets, err := NewTweets(auth, user, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = newTweets.UnRetweet()
+	for len(newTweets.Meta.NextToken) > 0 {
+		newTweets, err = NewTweets(auth, user, &newTweets.Meta.NextToken)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_ = newTweets.UnRetweet()
+	}
 }
